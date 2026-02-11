d07b939899944f8348d36f53befdcd73784f5081
RobotX23 <b.stepan.95@gmail.com>
2026-02-11 21:09:09 +0700

    Исправление запуска бота

--- InteractiveСonsole/Program.cs
+++ InteractiveСonsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using InteractiveСonsole;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 
 
 
@@ -8,13 +10,32 @@ try
 {
     using var ctr = new CancellationTokenSource();
     string token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN_EX1", EnvironmentVariableTarget.User);
+    if (string.IsNullOrEmpty(token))
+    {
+        Console.WriteLine("Токен бота не найден. Убедитесь, что вы установили переменную окружения.");
+        return;
+    }
+
     var userRepository = new InMemoryUserRepository();
     var toDoRepository = new InMemoryToDoRepository();
     var toDoService = new ToDoService(toDoRepository, 0, 0);
     var userService = new UserService(userRepository);
     var botClient = new TelegramBotClient(token);
+
+    // Настройка опций получения обновлений
+    var receiverOptions = new ReceiverOptions
+    {
+        AllowedUpdates = [UpdateType.Message], // Укажите необходимые типы обновлений, например, UpdateType.Message
+        DropPendingUpdates = true
+    };
     var handler = new UpdateHandler(toDoService, userService, toDoRepository, botClient);
+    // Начинаем получать обновления
     botClient.StartReceiving(handler);
+
+    var me = await botClient.GetMe();
+    Console.WriteLine($"{me.FirstName} запущен!");
+
+    await Task.Delay(-1); // Устанавливаем бесконечную задержку
 
 }
 catch (Exception ex)
