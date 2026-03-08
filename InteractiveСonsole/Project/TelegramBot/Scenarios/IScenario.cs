77a0536b2cc6e7f15b940094169152656773e705
RobotX23 <b.stepan.95@gmail.com>
2026-03-08 16:31:51 +0700

    Добавление поддержки сценариев

---
+++ InteractiveСonsole/Project/TelegramBot/Scenarios/IScenario.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace InteractiveСonsole.Project.TelegramBot.Scenarios
+{
+    internal interface IScenario
+    {
+        bool CanHandle(ScenarioType scenario);
+        Task<ScenarioResult> HandleMessageAsync(ITelegramBotClient bot, ScenarioContext context, Message message, CancellationToken ct);
+    }
+}
