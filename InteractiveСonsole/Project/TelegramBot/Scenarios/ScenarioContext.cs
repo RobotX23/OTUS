830e69a9c425ebdfc10b7a19e918cf9e5428a64c
RobotX23 <b.stepan.95@gmail.com>
2026-04-05 18:34:01 +0700

    Revert "Исправлено (контекст сценария DeleteList обновляется автоматичесик0"
    This reverts commit e3fd9f395a67f3cabcfc3bed8233cc485b30b537.

--- InteractiveСonsole/Project/TelegramBot/Scenarios/ScenarioContext.cs
+++ InteractiveСonsole/Project/TelegramBot/Scenarios/ScenarioContext.cs
@@ -2,23 +2,12 @@
 {
     internal class ScenarioContext
     {
-        public ScenarioType CurrentScenario { get; set; }
+        public ScenarioType CurrentScenario {  get; set; }
         public string? CurretStep { get; set; }
         public Dictionary<string, object> Data { get; set; } = new();
-
-        public ScenarioContext(ScenarioType scenario)
+        public ScenarioContext(ScenarioType scenario) 
         {
             CurrentScenario = scenario;
-        }
-
-        public void SetData(string key, object? value)
-        {
-            Data[key] = value!;
-        }
-
-        public T? GetData<T>(string key) where T : class
-        {
-            return Data.TryGetValue(key, out var value) ? value as T : null;
         }
     }
 }
